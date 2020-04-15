be6091233c580e0c55de1c0ef07073cb403fc1f6
Cpzty <Cristianpzty@gmail.com>
2020-04-15 15:45:45 -0600

    time to merge with proyect 2

---
+++ Tokens.py
@@ -0,0 +1,27 @@
+class Node():
+    def __init__(self):
+        self.data = ''
+        self.content = []
+        self.exceptions = []
+class Tokens:
+    def __init__(self):
+        self.characters = []
+        self.keywords = {}
+        self.tokens = []
+        self.productions = []
+        self.tokens_excepto = []
+    def create_node(self):
+        my_node = Node()
+        return my_node
+
+    def see_nodes(self, what_list):
+        if what_list != self.keywords:
+            for nod in what_list:
+                print('name: ', nod.data)
+                print('content: ', nod.content)
+                print('exceptions: ', nod.exceptions)
+        else:
+            print(what_list)
+
+
+
